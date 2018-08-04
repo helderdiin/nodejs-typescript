d7fbd2d5243c73e18d21d9e086e0b3480adad329
FIBBAURU <helderdiin.work@gmail.com>
2018-08-04 16:26:05 -0700

    Adding connection to mongo

--- lib/app.ts
+++ lib/app.ts
@@ -1,20 +1,27 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Routes } from './routes/crmRoutes';
+import * as mongoose from "mongoose";
 
 class App {
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://root:1234root@ds113402.mlab.com:13402/crmdb';
 
     constructor() {
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void {
+        mongoose.connect(this.mongoUrl);
     }
 }
 
