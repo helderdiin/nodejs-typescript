413737536acf0cab8f3f5fe2ac250d97d390eb4e
FIBBAURU <helderdiin.work@gmail.com>
2018-08-04 16:27:14 -0700

    Adding support to controller in routes for crm module

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,34 +1,16 @@
-import { Request, Response } from 'express';
+import { ContactController } from '../controllers/crmController';
 
 export class Routes {
+    public contactController: ContactController = new ContactController();
+
     public routes(app): void {
         app.route('/contact')
-            .get((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'success GET'
-                });
-            })
-            .post((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'success POST'
-                });
-            });
+            .get(this.contactController.getContacts)
+            .post(this.contactController.addNewContact);
 
         app.route('/contact/:contactId')
-            .get((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'success GET'
-                });
-            })
-            .put((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'success PUT'
-                });
-            })
-            .delete((req: Request, res: Response) => {
-                res.status(200).send({
-                    message: 'success DELETE'
-                });
-            });
+            .get(this.contactController.getContactWithID)
+            .put(this.contactController.updateContact)
+            .delete(this.contactController.deleteContact);
     }
 }
